ab2c1f380eb45d2628dd60e8dbdf5ba55d32263a
Menekse-Hasdemir <mammola.444@gmail.com>
2025-03-22 23:20:23 +0300

    görev3 tamam

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ function KareninAlani(kenaruzunlugu) {
 function CemberinCevresi(yaricap) {
  return `Çemberin çevresi = 2*pi*yaricap`
 }
+console.log(CemberinCevresi(5))
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
